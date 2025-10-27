4b6414c302ebb33d6ace6c737d1d7ed3f19ccf9b
Samir Vergara V <svergara3@misena.edu.co>
2025-10-26 20:46:02 -0500

    Update 2025-10-26

--- src/app/store/layouts/store-front-layout/store-front-layout.ts
+++ src/app/store/layouts/store-front-layout/store-front-layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 
 @Component({
   selector: 'app-store-front-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './store-front-layout.html',
 })
 export class StoreFrontLayout {}
--- src/app/store/store-front.routes.ts
+++ src/app/store/store-front.routes.ts
@@ -1,11 +1,32 @@
 import { Routes } from '@angular/router';
 import { StoreFrontLayout } from './layouts/store-front-layout/store-front-layout';
+import { HomePage } from './pages/home-page/home-page';
+import { ProductPage } from './pages/product-page/product-page';
+import { GenderPage } from './pages/gender-page/gender-page';
+import { NotFoundPage } from './pages/not-found-page/not-found-page';
 
-export const storeFrontRoutes: Routes = [
+const storeFrontRoutes: Routes = [
   {
     path: '',
     component: StoreFrontLayout,
-    children: [],
+    children: [
+      {
+        path: '',
+        component: HomePage,
+      },
+      {
+        path: 'gender/:gender',
+        component: GenderPage,
+      },
+      {
+        path: 'product/:id',
+        component: ProductPage,
+      },
+      {
+        path: '**',
+        component: NotFoundPage,
+      },
+    ],
   },
   {
     path: '**',
